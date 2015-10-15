5324fd07abb70b431d2fe9971a30e1e7a92ad9e1
redspidermkv <redspidermkv@hotmail.com>
2015-10-15 22:27:09 +0100

    updating example file

--- example.py
+++ example.py
@@ -9,11 +9,18 @@
 # Licence:     <your licence>
 #-------------------------------------------------------------------------------
 
-import YahooFinance.YahooApi as YahooApi
-from YahooFinance.YahooApi_Symbols import *
+import PresentDataLib.YahooFinance.YahooApi as YahooApi
+from PresentDataLib.YahooFinance.YahooApi_Symbols import *
 
-import GoogleFinance.GoogleApi as GoogleApi
-from GoogleFinance.GoogleApi_Symbols import *
+import PresentDataLib.GoogleFinance.GoogleApi as GoogleApi
+from PresentDataLib.GoogleFinance.GoogleApi_Symbols import *
+
+import HistoricDataLib.HistoricalQuote_GoogleAPI as HistoricalQuote_GoogleAPI
+import HistoricDataLib.HistoricalQuote_YahooAPI as HistoricalQuote_YahooAPI
+
+def printLines(lst):
+    for line in lst:
+        print line
 
 def main():
     financeApi = YahooApi.yahooFinance()
@@ -23,12 +30,22 @@ def main():
     Pricing.Ask, Pricing.Bid, Pricing.LastTradeWithTime)
 
     print(financeApi.GetData(stockList, symbolList))
+
+    print '\n'    
     
     api = GoogleApi.GoogleFinanceApi()
 
     stockList = ['MSFT', 'AAPL', 'BARC.L']
     symbolList = [Symbols.ID, Symbols.Index, Symbols.StockSymbol, Symbols.LastTradePrice]
     print api.GetStockData(stockList, symbolList)
+    
+    data = HistoricalQuote_GoogleAPI.HistoricalQuote()
+    printLines(data.GetData('NASDAQ:AAPL', '6-18-2014', '6-19-2014'))
+
+    print ''    
+    
+    data = HistoricalQuote_YahooAPI.HistoricalQuote()
+    printLines(data.GetData('AAPL', '6-18-14', '6-19-14'))
 
 if __name__ == '__main__':
     main()
